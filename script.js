e7b79232728619eede0aa62162eff00009168113
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2023-10-07 14:07:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// if-else by using Logical operator
+
+let age = 60
+let gender = "male"
+if(age>59 && gender==="male")
+{
+  console.log("Your are eligible for this exam")
+
+}
+else{
+  console.log("your are not eligible")
+}
+console.log("END")
+
+//output
+// - Your are eligible for this exam
+// - END
